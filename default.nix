8dd1a40ebf19df340b1f26aaedd48f046787c868
Felipe Cortez <github@felipecortez.net>
2022-09-19 10:29:34 -0300

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+      x86-darwin-pkgs = import <nixpkgs> {
+        system = "x86_64-darwin";
+        config = { allowUnfree = true; };
+      };
+in
+  pkgs.mkShell {
+    nativeBuildInputs = [ pkgs.jdk17_headless pkgs.clojure pkgs.babashka x86-darwin-pkgs.websocat ];
+}
